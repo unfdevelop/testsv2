7ecc1f494f9f2014906be6676d9faa395b7a1b5c
Evgeniy Konovalov <evgeny.74@inbox.ru>
2020-08-01 15:20:49 +0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Block from './blocks.svelte';
+	let blocks =[{color: "red", visible: true},{color: "green", visible: true},{color: "blue", visible: true}];
+	let showElem = i => {
+		blocks.forEach(el=> el.visible=true);
+		blocks[i].visible = false;
+	}
+</script>
+<style>
+	.components{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+	.elem1{
+		box-sizing: border-box;
+		border:1px solid black;
+		width: 100px;
+		height: 100px;
+	}
+	.items{
+	  display:flex;
+	  justify-content: space-between;
+	  flex-wrap: wrap;
+	  width: 320px;
+	}
+</style>
+<div class="components">
+
+
+	<div class="comp comp2">
+		<div class="items">
+			<Block {...blocks[0]} on:click={(e) => showElem(0)} />
+			<Block {...blocks[1]} on:click={(e) => showElem(1)} />
+			<Block {...blocks[2]} on:click={(e) => showElem(2)} />
+		</div>
+	</div>
+</div>
---
+++ src/blocks.svelte
@@ -0,0 +1,16 @@
+<script>
+export let color;
+export let visible;
+</script>
+<style>
+.item{
+  width: 100px;
+  opacity: 0;
+  height: 100px;
+  border: 1px solid black;
+}
+.item.active{
+  opacity: 1;
+}
+</style>
+<div class="item" class:active={visible} style="background-color: {color};" on:click></div>
