9eaa079957d2bd29caa12e12bba29ed6e49fa361
Evgeniy Konovalov <evgeny.74@inbox.ru>
2020-08-01 15:22:17 +0500

    up

--- src/App.svelte
+++ src/App.svelte
@@ -7,17 +7,6 @@
 	}
 </script>
 <style>
-	.components{
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-around;
-	}
-	.elem1{
-		box-sizing: border-box;
-		border:1px solid black;
-		width: 100px;
-		height: 100px;
-	}
 	.items{
 	  display:flex;
 	  justify-content: space-between;
@@ -25,14 +14,9 @@
 	  width: 320px;
 	}
 </style>
-<div class="components">
 
-
-	<div class="comp comp2">
-		<div class="items">
-			<Block {...blocks[0]} on:click={(e) => showElem(0)} />
-			<Block {...blocks[1]} on:click={(e) => showElem(1)} />
-			<Block {...blocks[2]} on:click={(e) => showElem(2)} />
-		</div>
-	</div>
+<div class="items">
+	<Block {...blocks[0]} on:click={(e) => showElem(0)} />
+	<Block {...blocks[1]} on:click={(e) => showElem(1)} />
+	<Block {...blocks[2]} on:click={(e) => showElem(2)} />
 </div>
